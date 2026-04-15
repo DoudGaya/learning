a19f102f623ee38599acaad84bbb61cd8a82547c
GHALI 0NE <69957154+The-Bully@users.noreply.github.com>
2026-04-15 02:55:31 +0100

    Create outputs.js

---
+++ outputs.js
@@ -0,0 +1,13 @@
+/* practicing how to use the alert, prompt and confirm */
+
+// using alert
+alert(" Ghali is the GOAT (Greatest of all time");
+
+//using prompt
+let answer = prompt("What's your name", name);
+
+alert(`${answer} is the GOAT`);
+
+//using confirm
+let isGoat = confirm("Are you the GOAT?");
+alert(isGOAT);
