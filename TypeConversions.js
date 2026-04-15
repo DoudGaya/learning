49810db62ff8fdb57f1ae7990e8248460070986c
GHALI 0NE <69957154+The-Bully@users.noreply.github.com>
2026-04-15 15:35:58 +0100

    Create TypeConversions.js

---
+++ TypeConversions.js
@@ -0,0 +1,20 @@
+/* playing around with type conversions*/
+
+let num = 123;
+let str = "Hello World";
+let bool = True;
+
+// string conversions
+strNum = String (num);
+strStr = String (str);
+strBool = String (bool);
+
+//Number conversions
+numStrNum = Number (strNum);
+numStrStr = Number (strStr);
+numStrBool = Number (strBool);
+
+//Boolean  Conversions 
+boolNum = Boolean (num);
+boolStr = Boolean (str);
+boolBool = Boolean (bool); 
