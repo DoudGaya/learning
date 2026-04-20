6f0130016d403d3f763fb850f1f071ebabe85576
GHALI 0NE <69957154+The-Bully@users.noreply.github.com>
2026-04-20 23:41:42 +0100

    Update conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,1 +1,31 @@
-conditionals
+/* playing around with conditional statements in JS */
+
+//task 1
+let num = prompt ("Enter a num:", '');
+
+if (num) {
+  alert ("Hello!");
+}
+
+// task 2 
+let lang = prompt ("What is the official name of Javascript?", '');
+
+if (lang == "ECMAScript"){
+  alert ("Right!");
+} else {
+  alert ("You don’t know? ECMAScript!");
+}
+
+// task 3 
+let num2 = prompt ("Enter a number");
+
+if (num2 > 0)
+{
+  alert ("1");
+} else if (num2 < 0){
+  alert ("-1");
+} else if (num2 == 0){
+  alert ("0");
+}
+
+//task 4 
