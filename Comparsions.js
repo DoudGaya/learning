fa5a78bf4728c714be532dda915c50e4c4c40c20
GHALI 0NE <69957154+The-Bully@users.noreply.github.com>
2026-04-20 14:22:13 +0100

    Update Comparsions.js

--- Comparsions.js
+++ Comparsions.js
@@ -1,1 +1,9 @@
+/* Playing around with Javascript comparisons*/
 
+alert (5 > 4) //returns true 
+alert ("apple" > "pineapple") // returns false
+alert ("2" > "12") // returns false
+alert (undefined == null) // returns false
+alert (undefined === null) //returns false 
+alert (null == "\n0\n") //returns false
+alert (null === +"\n0\n") //returns false 
